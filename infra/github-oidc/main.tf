060fd632a39d56406ba306ede29bd46832895222
Robert Jordan <robert@twingate.com>
2022-09-14 06:32:14 -0400

    updates

--- infra/github-oidc/main.tf
+++ infra/github-oidc/main.tf
@@ -8,11 +8,11 @@ module "github-oidc-provider" {
   repositories              = var.github_repos
   oidc_role_attach_policies = var.oidc_role_attach_policies
 }
-/*
+
 resource "github_actions_secret" "secret" {
   for_each        = toset(var.github_repos)
   repository      = each.value
   secret_name     = "AWS_ROLE_ARN"
   plaintext_value = module.github-oidc-provider.oidc_role
 }
-*/
+
