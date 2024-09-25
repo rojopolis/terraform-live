21435808ed82d9abdee89645b59d226e6d2e736d
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2024-09-25 10:32:36 -0700

    Update module.

--- infra/bucket/main.tf
+++ infra/bucket/main.tf
@@ -1,5 +1,4 @@
-module "bucket" {
-  source             = "github.com/bananalab/terraform-modules//modules/aws-s3-bucket?ref=v0.2.0"
-  enable_replication = false
-  logging_enabled    = false
+module "s3-bucket" {
+  source  = "app.terraform.io/banana-lab/s3-bucket/aws"
+  version = "1.0.0"
 }
