6375bb576e9b240ea6c62add1f56320e8341ccb9
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2024-09-25 10:44:31 -0700

    fix errors.

--- infra/bucket/main.tf
+++ infra/bucket/main.tf
@@ -1,4 +1,17 @@
+terraform {
+  cloud {
+    organization = "banana-lab"
+
+    workspaces {
+      name = "terraform-live"
+    }
+  }
+}
+
 module "s3-bucket" {
   source  = "app.terraform.io/banana-lab/s3-bucket/aws"
   version = "1.0.0"
+
+  enable_replication = false
+  logging_enabled    = false
 }
